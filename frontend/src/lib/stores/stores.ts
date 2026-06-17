35a9fc96e92c2fec26e543c91ed08a7828fde004
JBK2116 <jovani.badinga7@gmail.com>
2026-06-17 13:03:01 -0400

    feat(meeting): added meeting store to frontend
    - The meeting store serves as the central point for storing all user
      meeting data on the frontend

--- frontend/src/lib/stores/stores.ts
+++ frontend/src/lib/stores/stores.ts
@@ -1,5 +1,9 @@
+import type { MeetingIn } from '$lib/types/meeting';
 import type { User } from '$lib/types/user';
 import { writable } from 'svelte/store';
 
-// global user object used throughout the applicationapplicaiton
+// global user object used throughout the application
 export const user = writable<User | null>(null);
+
+// meetings received from the backend used throughout the application
+export const meetings = writable<MeetingIn[]>([]);
